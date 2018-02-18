c3a03e4875e47c3c7a9546298e8f63fd61607026
jsjlewis96 <jsjlewis96@gmail.com>
2018-02-18 18:23:14 +0000

    added support for redo button

--- src/main/java/controllers/menus/MenuBarController.java
+++ src/main/java/controllers/menus/MenuBarController.java
@@ -1,15 +1,13 @@
 package controllers.menus;
 
 import controllers.CommandDelegator;
-import controllers.editor.ConfigurationEditorController;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tooltip;
 import javafx.stage.Stage;
 
@@ -35,6 +33,9 @@ public class MenuBarController implements Initializable, InvalidationListener{
     @FXML
     Button undoBtn;
 
+    @FXML
+    Button redoBtn;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         FileMenuController fileMenuController = new FileMenuController(stage, fileMenu);
@@ -45,9 +46,21 @@ public class MenuBarController implements Initializable, InvalidationListener{
 
         //set disabled on initialisation. there should be nothing to undo
         undoBtn.setDisable(true);
+        redoBtn.setDisable(true);
+
         undoBtn.setOnAction(event -> {
             try {
                 boolean result = commandDelegator.undo();
+                //TODO handle if no executor is subscribed
+            } catch (Exception e) {
+                e.printStackTrace();
+                //TODO handle exception better
+            }
+        });
+
+        redoBtn.setOnAction(event -> {
+            try {
+                boolean result = commandDelegator.redo();
                 //TODO handle if no executor is subscribed
             } catch (Exception e) {
                 e.printStackTrace();
@@ -70,10 +83,23 @@ public class MenuBarController implements Initializable, InvalidationListener{
         }
     }
 
+    private void updateRedoUI() {
+        if (commandDelegator.canRedo()) {
+            redoBtn.setDisable(false);
+            Tooltip tooltip = redoBtn.getTooltip();
+            if (null != tooltip) {
+                tooltip.setText(commandDelegator.getRedoName());
+            }
+        } else {
+            redoBtn.setDisable(true);
+        }
+    }
+
     @Override
     public void invalidated(Observable observable) {
         if (observable instanceof CommandDelegator) {
             updateUndoUI();
+            updateRedoUI();
         }
     }
 }
